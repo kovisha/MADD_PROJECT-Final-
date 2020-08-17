9d14cd1e3b040aea93ca1c5c7578fa8e90b4b8c0
krushi1403 <kyadu9646@gmail.com>
2020-08-17 09:05:30 +0530

    BMI|BMR Tracker (KRUSHI : IT19162010) New

--- app/src/main/java/com/example/project_madd/User_profile.java
+++ app/src/main/java/com/example/project_madd/User_profile.java
@@ -2,13 +2,19 @@ package com.example.project_madd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class User_profile extends AppCompatActivity {
 
+
+    Dialog myDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +46,36 @@ public class User_profile extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    public void showPopup(View view){
+
+        Button confirm , cancel;
+
+        myDialog.setContentView(R.layout.popup);
+
+        confirm = myDialog.findViewById(R.id.btn_delete_yes);
+        cancel = myDialog.findViewById(R.id.btn_delete_no);
+
+        myDialog.show();
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myDialog.dismiss();
+            }
+        });
+
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(User_profile.this,User_profile.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 
 
